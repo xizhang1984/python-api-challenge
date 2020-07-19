65fbefb6efbc1c249c7c9b7ecaf27d5b709c7f12
xizhang <xzhan137@gmail.com>
2020-07-19 11:53:15 -0500

    without description

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "3bb9fa11a7b824682a38c5822d0f2b79"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCl531S6TdUUvoOKSd9PL9mTSCmJOBeK1M"
